6ad99f35e3316795d7cae3cf5dd1171433a716d1
Ryan W <79157185+Webbly25@users.noreply.github.com>
2022-05-13 11:32:37 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	const port = 3000;
+	await app.listen(port);
+
+	const url = await app.getUrl();
+	console.log(`Running at ${url}`);
+}
+bootstrap();
